56d5dead3471f636d61a8113bf0bebd3ef220e43
bang.nguyen2 <bangca85@gmail.com>
2024-08-06 11:45:22 +0700

    add init voice assistant project

---
+++ voice_assistant/worker.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+import requests
+
+openai_client = OpenAI()
+
+
+def speech_to_text(audio_binary):
+    return None
+
+
+def text_to_speech(text, voice=""):
+    return None
+
+
+def openai_process_message(user_message):
+    return None
